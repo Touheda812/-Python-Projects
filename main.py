9885439ad9d24a7e3c22e2387db548c688146d66
Touheda Khanom <57848483+Touheda812@users.noreply.github.com>
2023-01-27 14:34:29 +0000

    day-1-4-solution

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
-#This code prints the number of characters in a user's name.
-print( len( input("What is your name? ") ) )
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
 
+####################################
+#Write your code below this line 👇
 
-
-#Notes
-#If input was "Jack"
-#1st: print(len("Jack"))
-#2nd: print(4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+c = a
+a = b
+b = c
 
 
 #Write your code above this line 👆
-# 🚨 Do NOT modify the code below this line 👇
+####################################
 
-with open('testing_copy.py', 'w') as file:
-  file.write('def test_func():\n')
-  with open('main.py', 'r') as original:
-    f2 = original.readlines()[0:30]
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Testing code
+
+with open("testing_copy.py", "w") as file:
+  file.write("def test_func():\n")
+  with open("main.py", "r") as original:
+    f2 = original.readlines()[0:40]
     for x in f2:
       file.write("    " + x)
+
 
 import testing_copy
 import unittest
@@ -44,26 +54,24 @@ from io import StringIO
 import os
 
 class MyTest(unittest.TestCase):
-  def run_test(self, given_answer, expected_print): 
-    with patch('builtins.input', return_value=given_answer), patch('sys.stdout', new=StringIO()) as fake_out: 
+  def run_test(self, given_answer, expected_print):
+    with patch('builtins.input', side_effect=given_answer), patch('sys.stdout', new=StringIO()) as fake_out: 
       testing_copy.test_func()
       self.assertEqual(fake_out.getvalue(), expected_print) 
 
   def test_1(self):
-    self.run_test(given_answer='Jack', expected_print='4\n')
+    self.run_test(given_answer=['5', 'Y'], expected_print='a: Y\nb: 5\n')
 
   def test_2(self):
-    self.run_test(given_answer='Philipp', expected_print='7\n')  
+    self.run_test(given_answer=['97', '20'], expected_print='a: 20\nb: 97\n')
 
   def test_3(self):
-    self.run_test(given_answer='Ernest Hemingway', expected_print='16\n')
+    self.run_test(given_answer=['Python', 'Monty'], expected_print='a: Monty\nb: Python\n')
 
 print("\n\n\n.\n.\n.")
-print('Checking if your code works with several names...')
+print('Checking if you have printed the swapped values...')
 print('Running some tests on your code:')
 print(".\n.\n.")
 unittest.main(verbosity=1, exit=False)
 
 os.remove("testing_copy.py") 
-
-
